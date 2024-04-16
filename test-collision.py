5643959c3cd6c4081022ffefd64633ddfca1f1fa
Songtao Cheng <36098954+cccoolll@users.noreply.github.com>
2024-04-16 15:28:13 +0200

    🚀feat(robot) implement path execution and collision detection 🛠️refactor(code) optimize joint status logging ⚙️chore(files) update file paths and mesh locations

--- test-collision.py
+++ test-collision.py
@@ -1,94 +1,176 @@
 import pybullet as p
 import pybullet_data
-import numpy as np
 import time
-
-def matrix_to_quaternion(matrix):
-    """
-    Convert a 3x3 rotation matrix to a quaternion.
-    :param matrix: 3x3 rotation matrix as a numpy array.
-    :return: Quaternion as a list [x, y, z, w].
-    """
-    m = np.array(matrix, dtype=np.float64)
-    tr = np.trace(m)
-    if tr > 0:
-        S = np.sqrt(tr+1.0) * 2  # S=4*qw
-        qw = 0.25 * S
-        qx = (m[2, 1] - m[1, 2]) / S
-        qy = (m[0, 2] - m[2, 0]) / S
-        qz = (m[1, 0] - m[0, 1]) / S
-    elif (m[0, 0] > m[1, 1]) and (m[0, 0] > m[2, 2]):
-        S = np.sqrt(1.0 + m[0, 0] - m[1, 1] - m[2, 2]) * 2  # S=4*qx
-        qw = (m[2, 1] - m[1, 2]) / S
-        qx = 0.25 * S
-        qy = (m[0, 1] + m[1, 0]) / S
-        qz = (m[0, 2] + m[2, 0]) / S
-    elif m[1, 1] > m[2, 2]:
-        S = np.sqrt(1.0 + m[1, 1] - m[0, 0] - m[2, 2]) * 2  # S=4*qy
-        qw = (m[0, 2] - m[2, 0]) / S
-        qx = (m[0, 1] + m[1, 0]) / S
-        qy = 0.25 * S
-        qz = (m[1, 2] + m[2, 1]) / S
-    else:
-        S = np.sqrt(1.0 + m[2, 2] - m[0, 0] - m[1, 1]) * 2  # S=4*qz
-        qw = (m[1, 0] - m[0, 1]) / S
-        qx = (m[0, 2] + m[2, 0]) / S
-        qy = (m[1, 2] + m[2, 1]) / S
-        qz = 0.25 * S
-    return [qx, qy, qz, qw]
-
-def calculate_position(joint_xyz, joint_rpy, parent_xyz, parent_rpy):
-    """
-    Calculates the final position in the world coordinates.
-    """
-    parent_trans = p.getMatrixFromQuaternion(p.getQuaternionFromEuler(parent_rpy))
-    joint_trans = p.getMatrixFromQuaternion(p.getQuaternionFromEuler(joint_rpy))
-    
-    parent_trans = np.array(parent_trans).reshape(3, 3)
-    joint_trans = np.array(joint_trans).reshape(3, 3)
-    
-    world_trans = np.dot(parent_trans, joint_trans)
-    
-    world_quaternion = matrix_to_quaternion(world_trans)
-    
-    world_position = np.dot(parent_trans, np.array(joint_xyz)) + np.array(parent_xyz)
-    world_position = [world_position[0], world_position[1], world_position[2]]
-    #world_position = [world_position[0]+0.0393, world_position[1]-0.07, world_position[2]]
-    print(world_position)
-    
-    return world_position, world_quaternion
+import math
+import json
 
 # Connect to PyBullet
 p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0, 0, 0)
+wellplateNum = None
+
 flags = p.URDF_USE_SELF_COLLISION
-labId = p.loadURDF("C:/Users/songtao.cheng/Documents/myphd/imaging_farm/Digital Twin Lab/Digital_twin_lab/urdf/Digital_twin_lab.urdf", [0, 0, 0], useFixedBase=True, flags=flags)
+labId = p.loadURDF("C:/Users/songtao.cheng/Documents/myphd/imaging_farm/Digital Twin Lab/Digital_twin_lab-2/urdf/Digital_twin_lab-2.urdf", [0, 0, 0], useFixedBase=True, flags=flags)
 
-# Define joint and parent information for the position '1' from URDF (example values)
-joint_xyz = [-0.111219143031836, 0.29870945080481, -0.153551639442251]  # joint origin position for '1j'
-joint_rpy = [-1.57079, 1.57079, 0]  # joint origin orientation for '1j'
-parent_xyz = [-0.0325122942941528, -0.825674017096931, 0.0612003345061164]  # position of microplate_incubator
-parent_rpy = [1.5707963267949, 0, 0]  # orientation of microplate_incubator
 
-# Calculate the correct position
-world_position, world_orientation = calculate_position(joint_xyz, joint_rpy, parent_xyz, parent_rpy)
+def load_sequences_from_json(filepath):
+    with open(filepath, 'r') as file:
+        data = json.load(file)
+    return data['sequences']
 
-# Adjust the mesh scale and location according to the calculated position
+def import_positions(filename):
+    positions = []
+    with open(filename, 'r') as file:
+        for line in file:
+            parts = line.strip().split('xyz="')[1][:-1]  
+            float_coords = list(map(float, parts.split()))  
+            positions.append(float_coords)
+    return positions
+
+positions = import_positions('def-files/wellplate_pos.def')
+
+# Adjust mesh scale as needed and check visual representation
 collisionShapeId = p.createCollisionShape(shapeType=p.GEOM_MESH,
-                                          fileName="cube-meshes/96wellplate.stl",
-                                          meshScale=[0.001, 0.001, 0.001])
+                                          fileName="parts-meshes/1.stl",
+                                          meshScale=[1, 1, 1])  # Adjusted mesh scale
 
-wellplate = p.createMultiBody(baseCollisionShapeIndex=collisionShapeId,
-                           basePosition=world_position, # Use the calculated position
-                           baseOrientation=world_orientation) # Use the calculated orientation
+# Debugging: visual representation of the collision shape
+visualShapeId = p.createVisualShape(shapeType=p.GEOM_MESH,
+                                    fileName="parts-meshes/1.stl",
+                                    meshScale=[1, 1, 1],
+                                    rgbaColor=[1, 0, 0, 1],
+                                    specularColor=[0.4, 0.4, 0])
 
-collision_threshold = 0.003  # 3mm threshold for collision detection
+wellplates = []
+for position in positions:
+    wellplate = p.createMultiBody(baseMass=0,
+                                  baseCollisionShapeIndex=collisionShapeId,
+                                  baseVisualShapeIndex=visualShapeId,  # Added visual shape
+                                  basePosition=position,
+                                  baseOrientation=p.getQuaternionFromEuler([3.1416, 0, 1.5708]))
+    wellplates.append(wellplate)
 
-# Simulation loop
+
+def grab_plate(labId, plateId, gripperLinkIndex=7):
+    # Get initial absolute positions and orientations
+    gripperPos, gripperOrn = p.getLinkState(labId, gripperLinkIndex)[:2]
+    platePos, plateOrn = p.getBasePositionAndOrientation(plateId)
+    
+    # Calculate initial relative position and orientation
+    invGripperPos, invGripperOrn = p.invertTransform(gripperPos, gripperOrn)
+    global relativePos, relativeOrn
+    relativePos, relativeOrn = p.multiplyTransforms(invGripperPos, invGripperOrn, platePos, plateOrn)
+
+
+
+
+
+
+
+collision_threshold = 0.0001  
+
+def print_joint_status(robot_id):
+    num_joints = p.getNumJoints(robot_id)
+    for j in range(num_joints):
+        info = p.getJointState(robot_id, j)
+        if j >= 1:
+            print(f"Joint {j} status: Pos={math.degrees(info[0]):.3f}, Vel={info[1]:.3f}")
+        else:
+            print(f"Joint {j} status: Pos={info[0]:.3f}, Vel={info[1]:.3f}")
+
+def check_collision(lab_id, collision_threshold=0.0001):
+    contact_points = p.getContactPoints(bodyA=lab_id, bodyB=lab_id)
+    for contact in contact_points:
+        penetration_depth = contact[8]
+        if abs(penetration_depth) >= collision_threshold:  
+            collision_msg = f"Collision detected between parts."
+            print(collision_msg)
+            p.addUserDebugText(collision_msg, [0, 0, 0.5], textColorRGB=[1, 0, 0], textSize=1.5, lifeTime=3)
+            return True
+    return False
+
+def slide_to(lab_id, position, speed, wait_for_completion=True):
+    rail_joint_index = 1  
+    p.setJointMotorControl2(bodyUniqueId=lab_id,
+                            jointIndex=rail_joint_index,
+                            controlMode=p.POSITION_CONTROL,
+                            targetPosition=position,
+                            positionGain=speed)
+    if wait_for_completion:
+        current_position = p.getJointState(lab_id, rail_joint_index)[0]
+        while abs(current_position - position) > 0.001:  
+            p.stepSimulation()  
+            if check_collision(lab_id):
+                print("Collision detected, stopping execution.")
+                return True
+            current_position = p.getJointState(lab_id, rail_joint_index)[0]
+            time.sleep(1. / 120)
+
+def dorna_execute(lab_id,wellplates, sequence):
+    global wellplateNum
+    print(f"Executing sequence: {sequence['sequence_id']} - {sequence['description']}")
+    for action in sequence["actions"]:
+        print(f"Executing action: {action['cmd']}")
+        if action["cmd"] == "jmove":
+            joint_positions = [action["j0"], action["j1"], action["j2"], action["j3"], action["j4"]]
+            for i, joint_position in enumerate(joint_positions):
+                jointIndex = i + 2
+                targetPosition = math.radians(joint_position)
+                currentPosition = p.getJointState(lab_id, jointIndex)[0]
+
+                # Set the desired position with a high position gain to reduce oscillation
+                p.setJointMotorControl2(bodyUniqueId=lab_id,
+                                        jointIndex=jointIndex,
+                                        controlMode=p.POSITION_CONTROL,
+                                        targetPosition=targetPosition,
+                                        positionGain=0.1,  # Increased gain for tighter control
+                                        force=500)  # Max force applied to hold position
+
+                # Wait loop to ensure the joint reaches and holds the position
+                while abs(currentPosition - targetPosition) > 0.001:
+                    p.stepSimulation()
+                    currentPosition = p.getJointState(lab_id, jointIndex)[0]
+                    if check_collision(lab_id):
+                        print("Collision detected, stopping execution.")
+                        return  # Exiting the function early if a collision occurs
+                    time.sleep(1./120)
+        
+        
+        elif action["cmd"] == "grab_plate":
+            wellplateNum = action["plate_index"]
+            
+            grab_plate(lab_id, wellplates[wellplateNum])
+        
+        elif action["cmd"] == "slide_to":
+            slide_to(lab_id, action["position"], action["vel"])
+
+
+
+last_time = time.time()
+
+
+print('Slided to -1.2')
+sequences = load_sequences_from_json('def-files/dorna_path.json')
+for sequence in sequences:
+    dorna_execute(labId,wellplates, sequence)
+
+
+
 while True:
     p.stepSimulation()
-    contact_points = p.getContactPoints(bodyA=labId, bodyB=labId)
+    # Update gripper state
+    gripperPos, gripperOrn = p.getLinkState(labId, 7)[:2]
+    
+    # Calculate new absolute position for the plate
+    newPlatePos, newPlateOrn = p.multiplyTransforms(gripperPos, gripperOrn, relativePos, relativeOrn)
+    
+    # Set plate's new position and orientation
+    if wellplateNum is not None:
+        p.resetBasePositionAndOrientation(wellplates[wellplateNum], newPlatePos, newPlateOrn)
+    
+    contact_points = p.getContactPoints()
+    
     for contact in contact_points:
         penetration_depth = contact[8]
         if abs(penetration_depth) >= collision_threshold:
@@ -98,6 +180,12 @@ while True:
             bodyB_name = p.getBodyInfo(bodyB)[1].decode('utf-8')
             linkA_name = 'base' if linkA == -1 else p.getJointInfo(bodyA, linkA)[12].decode('utf-8')
             linkB_name = 'base' if linkB == -1 else p.getJointInfo(bodyB, linkB)[12].decode('utf-8')
+
             print(f"Collision detected between '{bodyA_name}:{linkA_name}' and '{bodyB_name}:{linkB_name}' with penetration depth of {abs(penetration_depth):.3f} meters.")
 
-    time.sleep(1. / 24)
+    current_time = time.time()
+    if current_time - last_time >= 3:
+        print_joint_status(labId)  
+        last_time = current_time  
+
+    time.sleep(1. / 120)
--- test.py
+++ test.py
@@ -2,24 +2,56 @@ import pybullet as p
 import pybullet_data
 import time
 import math
-from scipy.spatial.transform import Rotation as R
+import json
 
-def initialize_camera():
-    camera_distance = 3  # Distance from the camera to the look-at point
-    camera_yaw = 50  # Rotation around the vertical axis, in degrees
-    camera_pitch = -30  # Pitch angle. Positive values point the camera down
-    camera_target_position = [0, 0, 0.5]  # XYZ coordinates that the camera is looking at
-    
-    p.resetDebugVisualizerCamera(camera_distance, camera_yaw, camera_pitch, camera_target_position)
+# Connect to PyBullet
+p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0, 0, 0)
+#planeId = p.loadURDF("plane.urdf")
+flags = p.URDF_USE_SELF_COLLISION
+labId = p.loadURDF("C:/Users/songtao.cheng/Documents/myphd/imaging_farm/Digital Twin Lab/Digital_twin_lab-2/urdf/Digital_twin_lab-2.urdf", [0, 0, 0], useFixedBase=True, flags=flags)
 
-def initialize_simulation():
-    p.connect(p.GUI)
-    p.setAdditionalSearchPath(pybullet_data.getDataPath())
-    p.setGravity(0, 0, 0)
-    flags = p.URDF_USE_SELF_COLLISION
-    lab_id = p.loadURDF("SimulatedLab/urdf/SimulatedLab.urdf", [-1, 1, 0], useFixedBase=True, flags=flags)
-    initialize_camera()
-    return lab_id
+def load_sequences_from_json(filepath):
+    with open(filepath, 'r') as file:
+        data = json.load(file)
+    return data['sequences']
+
+def import_positions(filename):
+    positions = []
+    with open(filename, 'r') as file:
+        for line in file:
+            parts = line.strip().split('xyz="')[1][:-1]  # Split the line and remove the trailing quote
+            float_coords = list(map(float, parts.split()))  # Convert the string coordinates to floats
+            positions.append(float_coords)
+    return positions
+
+# Example usage
+positions = import_positions('def-files/wellplate_pos.def')
+
+collisionShapeId = p.createCollisionShape(shapeType=p.GEOM_MESH,
+                                          fileName="parts-meshes/1.stl",
+                                          meshScale=[1, 1, 1]) # Adjust mesh scale as needed
+
+wellplates = []
+for position in positions:
+    wellplate = p.createMultiBody(baseCollisionShapeIndex=collisionShapeId,
+                                  basePosition=position,  # Use positions from the list
+                                  baseOrientation=p.getQuaternionFromEuler([3.1416, 0, 1.5708]))
+    wellplates.append(wellplate)
+
+
+
+collision_threshold = 0.0001  # Threshold for collision detection
+
+def print_joint_status(robot_id):
+    num_joints = p.getNumJoints(robot_id)
+    for j in range(num_joints):
+        info = p.getJointState(robot_id, j)
+        if j >= 2:
+            print(f"Joint {j} status: Pos={math.degrees(info[0]):.3f}, Vel={info[1]:.3f}")
+        else:
+            print(f"Joint {j} status: Pos={info[0]:.3f}, Vel={info[1]:.3f}")
 
 def check_collision(lab_id, collision_threshold=0.0001):
     contact_points = p.getContactPoints(bodyA=lab_id, bodyB=lab_id)
@@ -40,87 +72,27 @@ def check_collision(lab_id, collision_threshold=0.0001):
             return True
     return False
 
-def slide_to(lab_id, position, speed, wait_for_completion=True):
-    rail_joint_index = 1 # Slide joint index
-    
-    p.setJointMotorControl2(bodyUniqueId=lab_id,
-                            jointIndex=rail_joint_index,
-                            controlMode=p.POSITION_CONTROL,
-                            targetPosition=position,
-                            positionGain=speed,
-                            )
-    if wait_for_completion:
-        current_position = p.getJointState(lab_id, rail_joint_index)[0]
-        while abs(current_position - position) > 0.001:  # Tolerance of 1mm
-            if check_collision(lab_id):
-                print("Collision detected, stopping execution.")
-                return True  # Return True to indicate collision
-            p.stepSimulation()
-            current_position = p.getJointState(lab_id, rail_joint_index)[0]
-            time.sleep(1. / 24.)  # Simulation step delay adjusted for more frequent checks
 
-def dorna_execute(lab_id, sequence):
-    print(f"Executing sequence: {sequence['sequence_id']} - {sequence['description']}")
-    for action in sequence["actions"]:
-        if action["cmd"] == "jmove":
-            joint_positions = [action["j0"], action["j1"], action["j2"], action["j3"], action["j4"]]
-            for i, joint_position in enumerate(joint_positions):
-                jointIndex = i + 3  # Starting from arm-base joint
-                targetPosition = math.radians(joint_position)
-                currentPosition = p.getJointState(lab_id, jointIndex)[0]
-                
-                # Move towards the target position with small increments
-                while abs(currentPosition - targetPosition) > 0.001:  # Tolerance of 1mm
-                    # Incremental step towards the target position
-                    step = max(min((targetPosition - currentPosition), 0.1), -0.1)
-                    p.setJointMotorControl2(bodyUniqueId=lab_id,
-                                            jointIndex=jointIndex,
-                                            controlMode=p.POSITION_CONTROL,
-                                            targetPosition=currentPosition + step,
-                                            positionGain=0.03
-                                            )
-                    p.stepSimulation()
-                    if check_collision(lab_id):
-                        print("Collision detected, stopping execution.")
-                        return  # Stop the entire sequence if collision detected
-                    currentPosition += step
-                    time.sleep(1. / 24.)  # Increased frequency of simulation steps for smoother movement
+last_time = time.time()
 
-def main():
-    lab_id = initialize_simulation()
-    time.sleep(10)  # Wait for simulation to load
-    if slide_to(lab_id, position=-1.2, speed=0.06):
-        print("Slide movement stopped due to collision.")
-        return  # Exit if collision occurred during sliding
-    print('Slided to -1.2')
-    
-    # Example sequence
-    sequences = [
-      {
-        "sequence_id": 1,
-        "description": "Preparation and Initial Positioning",
-        "note": ["This sequence is used to prepare the robot for the next steps."],
-        "actions": [
-            {"id":1,"cmd":"jmove","rel":0,"j0":5,"j1":-45,"j2":111.51123,"j3":10,"j4":-10,"vel":20,"accel":500,"jerk":2000}
-        ]
-      },
-     {
-        "sequence_id": 2,
-        "description": "Further Positioning",
-        "note": ["This sequence is used for further precise positioning."],
-        "actions": [
-            {"id":2,"cmd":"jmove","rel":0,"j0":5,"j1":45,"j2":411.51123,"j3":1.120605,"j4":-0.395508,"vel":20,"accel":500,"jerk":2000}
-        ]
-      }
-    ]
-    for sequence in sequences:
-        dorna_execute(lab_id, sequence)
-        time.sleep(1)  # Delay between sequences
-    
-    while True:
-        p.stepSimulation()
-        if check_collision(lab_id):
-            print("Collision detected.")
-        time.sleep(1. / 24.)  # Adjusted to match the frequency of checks
+while True:
+    p.stepSimulation()
+    contact_points = p.getContactPoints()
+    for contact in contact_points:
+        penetration_depth = contact[8]
+        if abs(penetration_depth) >= collision_threshold:
+            bodyA, bodyB = contact[1], contact[2]
+            linkA, linkB = contact[3], contact[4]
+            bodyA_name = p.getBodyInfo(bodyA)[1].decode('utf-8')
+            bodyB_name = p.getBodyInfo(bodyB)[1].decode('utf-8')
+            linkA_name = 'base' if linkA == -1 else p.getJointInfo(bodyA, linkA)[12].decode('utf-8')
+            linkB_name = 'base' if linkB == -1 else p.getJointInfo(bodyB, linkB)[12].decode('utf-8')
 
-main()
+            print(f"Collision detected between '{bodyA_name}:{linkA_name}' and '{bodyB_name}:{linkB_name}' with penetration depth of {abs(penetration_depth):.3f} meters.")
+
+    current_time = time.time()
+    if current_time - last_time >= 3:
+        print_joint_status(labId)  # Print the status of all joints for the robot every two seconds
+        last_time = current_time  # Reset the timer
+
+    time.sleep(1. / 24)  # Maintain the simulation step delay
